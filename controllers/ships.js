13c5e683258216ca9dbc33db537f9b9cf373f3eb
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-13 15:17:20 -0500

    complete ships index route

--- controllers/ships.js
+++ controllers/ships.js
@@ -2,6 +2,18 @@ import { Ship } from "../models/ship.js"
 
 function index(req, res){
   console.log("We go to ships!");
+  Ship.find({})
+  .then(ships => {
+    res.render('ships/index', {
+      ships,
+      title: "Ships",
+      user: req.user ? req.user : null, 
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
 }
 
 export {
