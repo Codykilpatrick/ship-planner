f00a0fbc553ae465873de7d7c97ea380f6547e6c
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-13 15:12:20 -0500

    add ui and route to go to ships page

---
+++ controllers/ships.js
@@ -0,0 +1,9 @@
+import { Ship } from "../models/ship.js"
+
+function index(req, res){
+  console.log("We go to ships!");
+}
+
+export {
+  index,
+}
--- routes/ships.js
+++ routes/ships.js
@@ -1,7 +1,15 @@
 import { Router } from "express"
 import * as shipCtrl from '../controllers/ships.js'
+import { isLoggedIn } from "../middleware/middleware.js"
+
 
 const router = Router()
+
+// localhost:3000/ships
+
+
+//GET localhost:3000/ships
+router.get('/', shipCtrl.index)
 
 export {
   router
