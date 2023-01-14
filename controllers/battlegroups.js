15eff4cb654ca771ae329fb6db5db4cd44d9634e
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-14 14:58:09 -0500

    complete functionality for bg index

--- controllers/battlegroups.js
+++ controllers/battlegroups.js
@@ -2,10 +2,21 @@ import { Battlegroup } from "../models/battlegroup.js"
 import { Ship } from "../models/ship.js"
 
 function index(req, res){
-  console.log("BG");
+  Battlegroup.find({})
+  .then(battlegroups => {
+    res.render('battlegroups/index', {
+      battlegroups,
+      title: "Battlegroups",
+      user: req.user ? req.user : null, 
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
 }
 
 export {
   index,
-  
+
 }
