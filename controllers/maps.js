d8dca55e1daf48b4a30002ef1a68b7ed4ff4d2a7
Cody Kilpatrick <2489375+Codykilpatrick@users.noreply.github.com>
2023-01-17 09:57:20 -0500

    create route and landing page for maps

---
+++ controllers/maps.js
@@ -0,0 +1,30 @@
+import { Battlegroup } from "../models/battlegroup.js"
+import { Ship } from "../models/ship.js"
+
+function index(req, res){
+  Battlegroup.find({})
+  .populate('ships')
+  .then(battlegroups => {
+    Ship.find({})
+    .then(ships => {
+      res.render('map', {
+        battlegroups,
+        ships,
+        title: "Map",
+        user: req.user ? req.user : null, 
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
+})
+.catch(error => {
+  console.log(error)
+  res.redirect('/')
+})
+}
+
+export {
+  index
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import * as mapCtrl from '../controllers/maps.js'
 
 const router = Router()
 
 router.get('/', function (req, res) {
   res.render('index', { title: 'Home Page' })
 })
+
+router.get('/map', mapCtrl.index)
 
 export {
   router
